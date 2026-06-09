0eae385030d0b8d8e5e0ad243ba489086df037cd
Ludovico Rossi <lurossi@c2r3s8.42roma.it>
2026-06-09 16:47:46 +0200

    last fix

--- parsing.py
+++ parsing.py
@@ -3,23 +3,25 @@ import io
 
 def file_parser(filename: str) -> tuple[dict, bool]:
     configuration = {}
+    keys_set = {"WIDTH", "HEIGHT", "ENTRY", "EXIT", "OUTPUT_FILE", "PERFECT", "SEED"}
     try:
         with open(filename) as file:
             one_line = file.read()
             one_line = one_line.rstrip("\n")
             actual_text = one_line.split("\n")
+            
             for line in actual_text:
                 if line == "" or line[0] == "#":
                     continue
                 else:
                     parsed_line = line.split("=", 1)
-                    if len(parsed_line) != 2 or parsed_line[0] not in ["WIDTH", "HEIGHT", "ENTRY", "EXIT", "OUTPUT_FILE", "PERFECT"]:
+                    if len(parsed_line) != 2 or parsed_line[0] not in keys_set:
                         raise ValueError("Wrong key input format")
                     else:
                         if parsed_line[0] == "WIDTH":
                             try:
                                 parsed_line[1] = int(parsed_line[1])
-                                if parsed_line[1] < 2 or "WIDTH" in configuration.keys():
+                                if parsed_line[1] < 1 or "WIDTH" in configuration.keys():
                                     raise ValueError("Wrong WIDTH input format")
                                 else:
                                     configuration[parsed_line[0]] = parsed_line[1]
@@ -28,7 +30,7 @@ def file_parser(filename: str) -> tuple[dict, bool]:
                         elif parsed_line[0] == "HEIGHT":
                             try:
                                 parsed_line[1] = int(parsed_line[1])
-                                if parsed_line[1] < 2 or "HEIGHT" in configuration.keys():
+                                if parsed_line[1] < 1 or "HEIGHT" in configuration.keys():
                                     raise ValueError("Wrong HEIGHT input format")
                                 else:
                                     configuration[parsed_line[0]] = parsed_line[1]
@@ -72,7 +74,19 @@ def file_parser(filename: str) -> tuple[dict, bool]:
                                 configuration[parsed_line[0]] = False
                             else:
                                 raise ValueError("Wront PERFECT input format")
-        return (configuration, True)
+                        if parsed_line[0] == "SEED":
+                            try:
+                                parsed_line[1] = int(parsed_line[1])
+                                if "SEED" in configuration.keys():
+                                    raise ValueError("Wrong SEED input format")
+                                else:
+                                    configuration[parsed_line[0]] = parsed_line[1]
+                            except Exception:
+                                raise ValueError("Wrong SEED input format")
+        if keys_set == set(configuration.keys()):
+            return (configuration, True)
+        else:
+            raise ValueError("Missing informations")
     except Exception as e:
         print(e)
         return (configuration, False)
